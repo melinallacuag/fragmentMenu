2b5354bc0a06329a09a0e1cdbbf5ea1f8dff7bd3
Melina <melina.llacua@gmail.com>
2022-12-05 17:33:58 -0500

    eliminar detalles

--- app/src/main/java/com/example/fragmentmenu/DetalleOpFacturaFragment.kt
+++ app/src/main/java/com/example/fragmentmenu/DetalleOpFacturaFragment.kt
@@ -5,29 +5,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.Navigation
+import kotlinx.android.synthetic.main.fragment_detalle_op_factura.*
+import kotlinx.android.synthetic.main.fragment_factura.*
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [DetalleOpFacturaFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class DetalleOpFacturaFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -36,24 +21,15 @@ class DetalleOpFacturaFragment : Fragment() {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_detalle_op_factura, container, false)
     }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment DetalleOpFacturaFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            DetalleOpFacturaFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+        btneliminar.setOnClickListener{
+            val showeliminar = EliminarOpFacturaFragment()
+            showeliminar.show((activity as AppCompatActivity).supportFragmentManager,"shop")
+
+        }
+
     }
+
 }
