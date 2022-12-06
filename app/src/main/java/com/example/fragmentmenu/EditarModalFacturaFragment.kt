36b0628c3918881a678115f90e9c1750e776253a
Melina <melina.llacua@gmail.com>
2022-12-06 09:44:23 -0500

    implementacion de su modal

---
+++ app/src/main/java/com/example/fragmentmenu/EditarModalFacturaFragment.kt
@@ -0,0 +1,24 @@
+package com.example.fragmentmenu
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.DialogFragment
+import kotlinx.android.synthetic.main.fragment_op_factura.*
+
+class EditarModalFacturaFragment : DialogFragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_editar_modal_factura, container, false)
+    }
+
+
+}
